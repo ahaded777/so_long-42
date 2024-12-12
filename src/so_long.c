fc3eb0b2de58806b95f290697f8a6e8d4e5ebf31
7NL <131011542+ahaded777@users.noreply.github.com>
2024-12-12 22:34:30 +0100

    Update so_long.c

--- src/so_long.c
+++ src/so_long.c
@@ -65,17 +65,12 @@ void render_map(void *mlx, void *mlx_win, t_textures textures, char **map, t_pla
                 mlx_put_image_to_window(mlx, mlx_win, textures.wall, j * TILE_SIZE, i * TILE_SIZE); 
             else if (map[i][j] == '0')
                 mlx_put_image_to_window(mlx, mlx_win, textures.floor, j * TILE_SIZE, i * TILE_SIZE);
-            else if (map[i][j] == 'P')
-                mlx_put_image_to_window(mlx, mlx_win, textures.player, j * TILE_SIZE, i * TILE_SIZE);
             else if (map[i][j] == 'C')
                 mlx_put_image_to_window(mlx, mlx_win, textures.coin, j * TILE_SIZE, i * TILE_SIZE);
             else if (map[i][j] == 'E')
                 mlx_put_image_to_window(mlx, mlx_win, textures.exit, j * TILE_SIZE, i * TILE_SIZE);
-            else if (i == player.y && j == player.x)
-            {
+            if (i == player.y && j == player.x)
                 mlx_put_image_to_window(mlx, mlx_win, textures.player, j * TILE_SIZE, i * TILE_SIZE);
-                printf("%d\n", player.x);
-            }
             j++;
         }
         i++;
@@ -101,39 +96,43 @@ void    size_map(char **map, t_map_size *map_size)
     map_size->len_x = x;
 }
 
-int    handle_keypress(int keycode, t_window *window)
+int handle_keypress(int keycode, t_window *window)
 {
-    t_player_move *player;
+    t_player_move *player = window->player;
 
-    player = window->player;
     if (keycode == ESC_KEY)
         exit(EXIT_SUCCESS);
-    if (keycode == 'D' || keycode == 'd' || keycode == RIGHT_KEY)
-    {
-        player->x += 1;
-    }
+    if (keycode == 'D' || keycode == 'd'|| keycode == RIGHT_KEY)
+        window->player->x += 1;
+    else if (keycode == 'A' || keycode == 'a'|| keycode == LEFT_KEY)
+        window->player->x -= 1;
+    else if (keycode == 'W' || keycode == 'w'|| keycode == UP_KEY)
+        window->player->y -= 1;
+    else if (keycode == 'S' || keycode == 's'|| keycode == DOWN_KEY)
+        window->player->y += 1;
+    mlx_clear_window(window->mlx, window->mlx_win);
+    render_map(window->mlx, window->mlx_win, window->textures, window->map, *player);
     return (0);
 }
 
 int main(int ac, char **av)
 {
-    t_window    window;
-    t_textures  textures;
-    t_map_size  map_size;
+    t_window        window;
+    t_textures      textures;
+    t_map_size      map_size;
     t_player_move   player;
-    char        **map;
-    
+
     if (ac != 2)
         print_message("Usage: ./so_long <map_file>\n", 2);
     window.mlx = mlx_init();
     if (!window.mlx)
         print_message("Error: x server\n", 2);
-    map = read_map(av[1]);
-    size_map(map, &map_size);
-    // player.x = 1;
-    // player.y = 1;
+    window.map = read_map(av[1]);
+    size_map(window.map, &map_size);
+    player.x = 1;
+    player.y = 1;
     window.player = &player;
-    window.mlx_win = mlx_new_window(window.mlx, map_size.len_x * WINDOW_WIDTH, map_size.len_y * WINDOW_WIDTH, "GTA SAN");
+    window.mlx_win = mlx_new_window(window.mlx, map_size.len_x * TILE_SIZE, map_size.len_y * TILE_SIZE, "GTA SAN");
     if (!window.mlx_win)
         print_message("Error: Failed to create window\n", 2);
     textures.wall = mlx_xpm_file_to_image(window.mlx, "./textures/wall.xpm", &window.img_width, &window.img_height);
@@ -141,11 +140,13 @@ int main(int ac, char **av)
     textures.player = mlx_xpm_file_to_image(window.mlx, "./textures/player_1.xpm", &window.img_width, &window.img_height);
     textures.coin = mlx_xpm_file_to_image(window.mlx, "./textures/coin.xpm", &window.img_width, &window.img_height);
     textures.exit = mlx_xpm_file_to_image(window.mlx, "./textures/exit.xpm", &window.img_width, &window.img_height);
-    if (!textures.wall || !textures.floor || !textures.player)
+    if (!textures.wall || !textures.floor || !textures.player || !textures.coin || !textures.exit)
         print_message("Error: Failed to load textures\n", 2);
-    render_map(window.mlx, window.mlx_win, textures, map, player);
+    window.textures = textures;
+    render_map(window.mlx, window.mlx_win, textures, window.map, player);
     mlx_hook(window.mlx_win, 17, 0, close_window, NULL);
     mlx_key_hook(window.mlx_win, handle_keypress, &window);
     mlx_loop(window.mlx);
+
     exit(EXIT_SUCCESS);
 }
